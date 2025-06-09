86f7cc0107bf2c5ad6c00c27e8d796c46ca300d3
vilhola2 <114490686+vilhola2@users.noreply.github.com>
2025-06-09 12:29:09 +0300

    Update intfmt.h

--- include/plib/intfmt.h
+++ include/plib/intfmt.h
@@ -6,8 +6,23 @@
 /**
  * @defgroup fmt_macros Integer format specifier macros
  *
- * These macros are wrappers for the fixed-with integer format specifiers defined in <inttypes.h>
+ * These macros are wrappers for the fixed-width integer format specifiers defined in <inttypes.h>
  *
+ * Naming convention:
+ *   - W = Write (printf-style)
+ *   - R = Read  (scanf-style)
+ *   - D = Decimal (signed)
+ *   - U = Unsigned
+ *   - x/X = Hex (lower/upper)
+ *   - 8/16/32/64/MAX/PTR = Bit width
+ * @{
+ */
+
+
+/**
+ * @defgroup write_macros Write macros
+ *
+ * These are for printf-style writes.
  * @{
  */
 
@@ -74,6 +89,86 @@
 /**
  * @}
  */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup read_macros Read macros
+ *
+ * These are for scanf-style reads.
+ * @{
+ */
+
+/**
+ * @defgroup signed_types Signed integer types
+ * @{
+ */
+
+#define RD8   "%" SCNd8
+#define RD16  "%" SCNd16
+#define RD32  "%" SCNd32
+#define RD64  "%" SCNd64
+#define RDMAX "%" SCNdMAX
+#define RDPTR "%" SCNdPTR
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup unsigned_types Unsigned integer types
+ * @{
+ */
+
+#define RU8   "%" SCNu8
+#define RU16  "%" SCNu16
+#define RU32  "%" SCNu32
+#define RU64  "%" SCNu64
+#define RUMAX "%" SCNuMAX
+#define RUPTR "%" SCNuPTR
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup uppercase_hex Uppercase hexadecimal
+ * @{
+ */
+
+#define RX8   "%" SCNX8
+#define RX16  "%" SCNX16
+#define RX32  "%" SCNX32
+#define RX64  "%" SCNX64
+#define RXPTR "%" SCNXPTR
+#define RXMAX "%" SCNXMAX
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup lowercase_hex Lowercase hexadecimal
+ * @{
+ */
+
+#define Rx8   "%" SCNx8
+#define Rx16  "%" SCNx16
+#define Rx32  "%" SCNx32
+#define Rx64  "%" SCNx64
+#define RxPTR "%" SCNxPTR
+#define RxMAX "%" SCNxMAX
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
 
 /**
  * @}
