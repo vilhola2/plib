378455f631275af41895a2d47136ff1d943202dd
vilhola2 <voipakeetti@gmail.com>
2025-12-21 21:25:23 +0200

    updarte

--- include/plib/thread.h
+++ include/plib/thread.h
@@ -7,18 +7,29 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-typedef HANDLE thread_t;
+typedef struct {
+    HANDLE handle;
+    void *(*func)(void *arg);
+    void *arg, *retval;
+} thread_t;
+
+typedef HANDLE mutex_t;
 
 #else
 
 #include <pthread.h>
 typedef pthread_t thread_t;
+typedef pthread_mutex_t mutex_t;
 
 #endif
 
 bool thread_create(thread_t *t, void *(*func)(void *arg), void *arg);
-bool thread_join(thread_t *thread);
+void *thread_join(thread_t *thread);
 bool thread_detach(thread_t *thread);
+bool mutex_create(mutex_t *mutex);
+bool mutex_destroy(mutex_t *mutex);
+bool mutex_lock(mutex_t *mutex);
+bool mutex_unlock(mutex_t *mutex);
 
 #endif
 
--- src/thread.c
+++ src/thread.c
@@ -6,37 +6,53 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
-struct thread_internal {
-    void *(*func)(void *arg);
-    void *arg;
-};
+#include <process.h>
 
 DWORD WINAPI thread_start(LPVOID arg) {
-    struct thread_internal *thread = (struct thread_internal *)arg;
-    thread->func(thread->arg);
-    free(thread);
+    thread_t *new_thread = (thread_t *)arg;
+    new_thread->retval = new_thread->func(new_thread->arg);
     return 0;
 }
 
 bool thread_create(thread_t *thread, void *(*func)(void *arg), void *arg) {
-    struct thread_internal *new_thread = malloc(sizeof(struct thread_internal));
-    if (unlikely(!new_thread)) return false;
-    new_thread->arg = arg;
-    new_thread->func = func;
-    *thread = CreateThread(NULL, 0, thread_start, new_thread, 0, NULL);
-    if (unlikely(!(*thread))) return false;
+    thread->arg = arg;
+    thread->func = func;
+    thread->handle = (HANDLE)_beginthreadex(NULL, 0, thread_start, thread, 0, NULL);
+    if (unlikely(!(thread->handle))) return false;
     return true;
 }
 
-bool thread_join(thread_t *thread) {
-    if (unlikely(WaitForSingleObject(*thread, INFINITE) != WAIT_OBJECT_0)) return false;
-    CloseHandle(*thread);
-    return true;
+void *thread_join(thread_t *thread) {
+    if (unlikely(WaitForSingleObject(thread->handle, INFINITE) != WAIT_OBJECT_0)) return NULL;
+    CloseHandle(thread->handle);
+    return thread->retval;
 }
 
 bool thread_detach(thread_t *thread) {
-    if (unlikely(!CloseHandle(*thread))) return false;
+    if (unlikely(!CloseHandle(thread->handle))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_create(mutex_t *mutex) {
+    if(unlikely(!(*mutex = CreateMutex(NULL, FALSE, NULL)))) {
+        fprintf(stderr, "CreateMutex error: %d\n", GetLastError());
+        return false;
+    }
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_destroy(mutex_t *mutex) {
+    if (unlikely(!CloseHandle(*mutex))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_lock(mutex_t *mutex) {
+    if (unlikely(WaitForSingleObject(*mutex, INFINITE) != WAIT_OBJECT_0)) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_unlock(mutex_t *mutex) {
+    if (unlikely(!ReleaseMutex(*mutex))) return false;
     return true;
 }
 
@@ -49,13 +65,34 @@ ALWAYS_INLINE bool thread_create(thread_t *thread, void *(*func)(void *arg), voi
     return true;
 }
 
-ALWAYS_INLINE bool thread_join(thread_t *thread) {
-    if (unlikely(pthread_join(*thread, NULL))) return false;
-    return true;
+ALWAYS_INLINE void *thread_join(thread_t *thread) {
+    void *retval;
+    if (unlikely(pthread_join(*thread, &retval))) return NULL;
+    return retval;
 }
 
 ALWAYS_INLINE bool thread_detach(thread_t *thread) {
     if (unlikely(pthread_detach(*thread))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_create(mutex_t *mutex) {
+    if (unlikely(pthread_mutex_init(mutex, NULL))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_destroy(mutex_t *mutex) {
+    if (unlikely(pthread_mutex_destroy(mutex))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_lock(mutex_t *mutex) {
+    if (unlikely(pthread_mutex_lock(mutex))) return false;
+    return true;
+}
+
+ALWAYS_INLINE bool mutex_unlock(mutex_t *mutex) {
+    if (unlikely(pthread_mutex_unlock(mutex))) return false;
     return true;
 }
 
